eb8d7eb4f6a6dc8d64d123812b1caef5b7c55bea
Ganesh Raju Galla <ganeshrajugalla@gmail.com>
2025-04-19 08:49:43 +0530

    Remove Git LFS config and local Frameworks folder

--- Frameworks/Jumio.xcframework/ios-arm64_x86_64-simulator/Jumio.framework/Headers/Jumio-Swift.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:48c2a0bf9e895a2244b459045b48be570b245d4ca1f75abe2dd3e8f6761bb886
-size 36721
--- Frameworks/JumioDefaultUI.xcframework/ios-arm64/JumioDefaultUI.framework/Headers/JumioDefaultUI-Swift.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e80854fb78b22f21fff6851d0541e7a0b410a5649b4cf91357b9416385276563
-size 10059
